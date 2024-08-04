4a0128fc3caa0ef36c7450fb7f97f5788a1dcb8a
Vignesh Suresh <61021342+Vignesh424@users.noreply.github.com>
2024-08-04 21:30:58 +0530

    Add files via upload

---
+++ MobilePrice.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pickle
+
+model = pickle.load(open('mobile.pkl', 'rb'), encoding='latin1')
+
+def run():
+    st.title("Mobile Prediction using Machine Learning")
+
+
+     ## Ratings
+    ratings = st.number_input('Enter Ratings')
+
+    ## RAM
+    ram = st.number_input('Enter RAM')
+
+    ## ROM
+    rom = st.number_input('Enter ROM')
+    
+    ## Mobile_Size
+    mob_size = st.number_input('Enter Mobile Size', value=0)
+
+    ## Number of pixels of the Back camera
+    backcam = st.number_input("Enter Pixels in Back Camera",value=0)
+
+    ## Number of pixels of the Selfi camera
+    selficam = st.number_input("Enter Pixels in Selfi Camera",value=0)
+
+    ## Battery
+    battery= st.number_input("Enter Battery",value=0)
+
+
+
+
+    if st.button("Submit"):
+        features = [[ratings,ram,rom,mob_size,backcam,selficam,battery]]
+        print(features)
+        prediction = model.predict(features)
+        weight = [str(i) for i in prediction]
+        ans = weight
+        if ans == 0:
+            st.error('Error')
+        else:
+            st.success(ans)
+            
+
+run()
